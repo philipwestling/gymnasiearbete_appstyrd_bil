057a64b6e9b68a425d22885560b5f56aa78beacc
PhilipWestling <punyaa2004@gmail.com>
2022-10-07 13:48:07 +0200

    Startskärm

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Appstyrd Bil',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const DeciderScreen(),
+    ),
+  );
+}
+
+class DeciderScreen extends StatelessWidget {
+  const DeciderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
