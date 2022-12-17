086e745955bcada35232923547211647f199074c
PhilipWestling <punyaa2004@gmail.com>
2022-12-17 22:39:25 +0100

    fungerade inte

--- Mobilapplikation/android/app/src/main/kotlin/com/example/gymnasiearbete_appstyrd_bil/MainActivity.kt
+++ Mobilapplikation/android/app/src/main/kotlin/com/example/gymnasiearbete_appstyrd_bil/MainActivity.kt
@@ -4,14 +4,6 @@ import io.flutter.embedding.android.FlutterActivity
 
 
 class MainActivity: FlutterActivity() {
-    RxJavaPlugins.setErrorHandler { throwable ->
-  if (throwable is UndeliverableException && throwable.cause is BleException) {
-    return@setErrorHandler // ignore BleExceptions since we do not have subscriber
-  }
-  else {
-    throw throwable
-  }
-}
 }
 
-
+ 
