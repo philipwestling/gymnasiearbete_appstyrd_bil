960d0122c1e3d9a624dbc2b1626113a06d2550b8
PhilipWestling <punyaa2004@gmail.com>
2022-12-22 10:21:19 +0100

    avsluta bluetoothanslutning början

--- Mobilapplikation/lib/screens/car_controls_screen.dart
+++ Mobilapplikation/lib/screens/car_controls_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gymnasiearbete_appstyrd_bil/bluetooth/methods.dart';
+import 'package:gymnasiearbete_appstyrd_bil/bluetooth/setup.dart';
+import 'package:gymnasiearbete_appstyrd_bil/constants/menu_action.dart';
+import 'package:gymnasiearbete_appstyrd_bil/constants/routes.dart';
 import 'package:gymnasiearbete_appstyrd_bil/widgets/gears.dart';
-import 'package:gymnasiearbete_appstyrd_bil/widgets/light_buttons.dart';
 import 'package:gymnasiearbete_appstyrd_bil/widgets/pedals.dart';
 import 'package:gymnasiearbete_appstyrd_bil/widgets/joystick.dart';
 import 'dart:developer' as dev_tools show log;
@@ -24,6 +26,7 @@ class _CarControlsState extends State<CarControls> {
   }
 
   bool isHighbeamActivated = false;
+  bool shouldDisconnect = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,58 +37,87 @@ class _CarControlsState extends State<CarControls> {
         shadowColor: Colors.white,
         actions: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              IconButton(
-                onPressed: () async {
-                  sendToUnoWifi(8);
-                },
-                // https://www.flaticon.com/free-icon/turn-signal_3674908?term=turn+signal&page=1&position=39&origin=search&related_id=3674908
-                icon: Image.asset("lib/assets/images/turn_signal_left.png"),
-                iconSize: 40,
+              PopupMenuButton<MenuAction>(
+                onSelected: ((value) async {
+                  switch (value) {
+                    case MenuAction.disconnect:
+                      shouldDisconnect = await showLogOutDialog(context);
+
+                      if (shouldDisconnect) {
+                        await scanStream.cancel();
+                        foundBluetoothDevicesList.clear();
+                        await bluetoothMonitor.flutterReactiveBle
+                            .deinitialize();
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            findDevicesRoute, (_) => false);
+                      }
+                      break;
+                  }
+                }),
+                itemBuilder: ((context) {
+                  return const [
+                    PopupMenuItem<MenuAction>(
+                      value: MenuAction.disconnect,
+                      child: Text("Avbryt Bluetoothanslutning"),
+                    ),
+                  ];
+                }),
               ),
-              IconButton(
-                onPressed: () async {
-                  sendToUnoWifi(9);
-                },
-                // https://www.flaticon.com/free-icon/turn-signal_3674908?term=turn+signal&page=1&position=39&origin=search&related_id=3674908
-                icon: Image.asset("lib/assets/images/turn_signal_right.png"),
-                iconSize: 40,
-              ),
-              IconButton(
-                onPressed: () async {
-                  setState(() {
-                    isHighbeamActivated = !isHighbeamActivated;
-                  });
-                  sendToUnoWifi(7);
-                },
-                // https://www.flaticon.com/free-icon/high-beam_95138
-                icon: isHighbeamActivated
-                    ? Image.asset("lib/assets/images/high_beam_activated2.png")
-                    : Image.asset(
-                        "lib/assets/images/high_beam_deactivated.png"),
-                iconSize: 40,
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.settings),
-                color: Colors.blueGrey,
-                iconSize: 30,
+              const SizedBox(
+                width: 24.5,
               ),
             ],
           )
         ],
+        leadingWidth: 218.5,
+        leading: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(padding: EdgeInsets.only(left: 48)),
+            IconButton(
+              onPressed: () async {
+                sendToUnoWifi(8);
+              },
+              // https://www.flaticon.com/free-icon/turn-signal_3674908?term=turn+signal&page=1&position=39&origin=search&related_id=3674908
+              icon: Image.asset("lib/assets/images/turn_signal_left.png"),
+              iconSize: 40,
+            ),
+            IconButton(
+              onPressed: () async {
+                sendToUnoWifi(9);
+              },
+              // https://www.flaticon.com/free-icon/turn-signal_3674908?term=turn+signal&page=1&position=39&origin=search&related_id=3674908
+              icon: Image.asset("lib/assets/images/turn_signal_right.png"),
+              iconSize: 40,
+            ),
+            IconButton(
+              onPressed: () async {
+                setState(() {
+                  isHighbeamActivated = !isHighbeamActivated;
+                });
+                sendToUnoWifi(7);
+              },
+              // https://www.flaticon.com/free-icon/high-beam_95138
+              icon: isHighbeamActivated
+                  ? Image.asset("lib/assets/images/high_beam_activated2.png")
+                  : Image.asset("lib/assets/images/high_beam_deactivated.png"),
+              iconSize: 40,
+            ),
+          ],
+        ),
       ),
-      backgroundColor: Color.fromARGB(250, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(250, 255, 255, 255),
       body: Container(
         margin: const EdgeInsets.fromLTRB(0, 110, 0, 0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Gas, broms och styrning
+            // Joystick, Pedaler, Växlar
             Row(
               children: [
-                carSteeringWidget(context),
+                const Padding(padding: EdgeInsets.only(left: 4.5)),
+                joystickWidget(context),
                 const SizedBox(
                   width: 400,
                 ),
@@ -101,4 +133,38 @@ class _CarControlsState extends State<CarControls> {
       ),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Avsluta bluetoothanslutning"),
+        content: Text(
+            "Vill du avsluta bluetoothanslutningen med ${deviceOfInterest.name}?"),
+        actionsAlignment: MainAxisAlignment.center,
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text(
+              'Ja',
+              style: TextStyle(color: Colors.green),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text(
+              'Nej',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
--- Mobilapplikation/lib/widgets/joystick.dart
+++ Mobilapplikation/lib/widgets/joystick.dart
@@ -4,7 +4,7 @@ import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:gymnasiearbete_appstyrd_bil/bluetooth/methods.dart';
 import 'package:num_remap/num_remap.dart';
 
-Widget carSteeringWidget(BuildContext context) {
+Widget joystickWidget(BuildContext context) {
   double leftOrRight;
   double mappedLeftOrRight;
 
@@ -15,6 +15,7 @@ Widget carSteeringWidget(BuildContext context) {
         period: const Duration(milliseconds: 200),
         listener: ((details) {
           leftOrRight = double.parse(details.x.toStringAsFixed(2));
+          print(leftOrRight);
           mappedLeftOrRight =
               mappedLeftOrRight = leftOrRight.remap(1, -1, 80, 160);
 
